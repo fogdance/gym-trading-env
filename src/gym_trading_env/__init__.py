b7cd7c7ab0914ebc2ed370d9ffea848b8565df18
fogdance <kunming.xie@hotmail.com>
2025-03-14 15:51:00 +0800

    fix: dreamerv3 train

--- src/gym_trading_env/__init__.py
+++ src/gym_trading_env/__init__.py
@@ -1,2 +1,2 @@
 # src/gym_trading_env/__init__.py
-
+from . import envs
--- src/gym_trading_env/envs/__init__.py
+++ src/gym_trading_env/envs/__init__.py
@@ -1,16 +1,8 @@
-# src/gym_trading_env.envs.__init__.py
-
+# src/gym_trading_env.envs/__init__.py
 from gymnasium.envs.registration import register
-from gym_trading_env.envs.trading_env import CustomTradingEnv
 
-def make_custom_trading_env():
-    def _env_creator(config_path=None):
-        return CustomTradingEnv(config_path=config_path)
-    return _env_creator
-
-# Register your custom trading environment with the factory function
 register(
-    id='CustomTradingEnv-v0',  # The environment ID
-    entry_point=make_custom_trading_env(),  # Pass the factory function
+    id='CustomTradingEnv-v0',
+    entry_point='gym_trading_env.envs.trading_env:CustomTradingEnv',
+    kwargs={'config_path': None},
 )
-
